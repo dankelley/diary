162c17796699c2bc4185a3390f0f7e0e717266f1
dankelley <kelley.dan@gmail.com>
2024-06-25 15:34:55 -0300

    wip

--- diary/main.py
+++ diary/main.py
@@ -5,7 +5,7 @@ import argparse
 import sys
 import json
 import os
-import re
+from re import split
 import textwrap
 import datetime
 import tempfile
@@ -18,31 +18,25 @@ showRandomHint = False
 defaultDatabase = "~/Dropbox/diary.db"
 
 def diary():
-
-    # If second arg is a number, it is a noteId
-    id_desired = None
-    print("len(sys.argv) = %d" % len(sys.argv))
-    if len(sys.argv) > 1:
-        try:
-            id_desired = sys.argv[1]
-            #del sys.argv[1]
-        except:
-            pass
-
+    #print("len(sys.argv) = %d" % len(sys.argv))
     parser = argparse.ArgumentParser(prog="diary", description="Diary: a diary tool",
             formatter_class=argparse.RawDescriptionHelpFormatter,
             epilog=textwrap.dedent("FIXME: explain more here"))
     parser.add_argument("item", type=str, help="Diary item")
     parser.add_argument("categories", type=str, help="Category (use comma to separate)")
     parser.add_argument("--debug", type=int, default=0,
-                        help="0 for quite action, 1 for information",
-                        metavar="de")
+                        help="0 for quiet action, 1 for some debugging output",
+                        metavar="level")
     parser.add_argument("--database", type=str, default=None,
-                        help="database location; defaults to %s" % defaultDatabase,
-                        metavar="db")
+                        help="database location (defaults to %s)" % defaultDatabase,
+                        metavar="filename")
     args = parser.parse_args()
+    if args.database:
+        print("user gave database '%s'" % args.database)
     if not args.database:
         args.database = defaultDatabase
-    print(args.item)
-    print(args.categories)
+    print("item '%s'" % args.item)
+    #print("categories '%s'" % args.categories)
+    categories = [category.strip() for category in args.categories.split(',')]
+    print("categories %s" % categories)
     diary = Diary(debug=args.debug, db=args.database)
