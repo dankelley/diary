87c72cacbb05226ba0c1c468a7bd976d403556ac
dankelley <kelley.dan@gmail.com>
2024-06-29 09:03:53 -0300

    cleanup

--- a.sql
+++
@@ -1,1 +0,0 @@
-SELECT entries.entryId, entries.time, entries.entry, tags.tag FROM entries JOIN entry_tag ON entry_tag.entryId = entries.entryId JOIN tags ON entry_tag.tagId = tags.tagId;
--- b.sql
+++
@@ -1,2 +0,0 @@
-SELECT tags.tag FROM tags JOIN entry_tag WHERE entry_tag.entryId = 1;
-#SELECT tag FROM tags;
